3e92713d8a20a9946af141df6ba465b5e619a5ff
Crypto733 <williamtoepfer101@gmail.com>
2020-03-07 21:14:49 +0100

    login databas

---
+++ app/src/main/java/com/example/theretrocourse/DatabaseOperation.java
@@ -0,0 +1,43 @@
+package com.example.theretrocourse;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.jar.Attributes;
+
+public class DatabaseOperation extends SQLiteOpenHelper {
+    public static final String database_name ="login.db";
+    public static final String table_name ="login_table";
+    public static final String col_1 ="BTH_mail";
+    public static final String col_2 ="Password";
+    //man kan också lägga till ID autoincrement som håller koll på registrationerna
+
+    public DatabaseOperation(Context context){
+        super(context, database_name, null, 1);
+
+    }
+
+    public void onCreate( SQLiteDatabase db){
+        db.execSQL("create table "+ table_name+"(BTH_mail VARCHAR PRIMARY KEY,Password TEXT)");
+    }
+
+    public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS "+ table_name);
+        onCreate(db);
+    }
+    public boolean insertLoginData(String BTHmail, String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_1,BTHmail);
+        contentValues.put(col_2,name);
+        long result =db.insert(table_name,null,contentValues);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+}
--- app/src/main/java/com/example/theretrocourse/Login.java
+++ app/src/main/java/com/example/theretrocourse/Login.java
@@ -6,29 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
     Intent intent;
+    EditText e_name,e_pass;
+    Button Login;
+    DatabaseOperation mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button Login = findViewById(R.id.login);
+        e_name=(EditText)findViewById(R.id.editText);
+        e_pass=(EditText)findViewById(R.id.editText2);
+        Login = findViewById(R.id.login);
         Login.setOnClickListener(this);
+        mydb= new DatabaseOperation(this);
+    }
+    public void addData(){
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = mydb.insertLoginData(e_name.getText().toString(),e_pass.getText().toString());
+                if(isInserted=true)
+                    Toast.makeText(Login.this,"Data inserted!", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(Login.this,"Error: data not inserted", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
-    //här behöver vi göra så att programmet tar fram rätt frame för student,teacher eller admin
     public void onClick(View v) {
         if(MainActivity.type=="s"){
+            addData();
             intent = new Intent(this,StudentMainPage.class);
             startActivity(intent);
         }
         if(MainActivity.type=="e"){
+            addData();
             intent = new Intent(this,TeacherMainPAge.class);
             startActivity(intent);
         }
         if(MainActivity.type=="a"){
+            addData();
             intent = new Intent(this,Creation.class);
             startActivity(intent);
         }
